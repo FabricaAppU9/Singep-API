813aa55dd226f351421e8a3b75e975ac0b28d237
Rodolfo1997 <rodolfostopinto@hotmail.com>
2017-09-16 20:07:58 -0300

    API e Admin Singep

--- README.md
+++
@@ -1,2 +0,0 @@
-# Singep-API
-Singep API
---
+++ singep/api/config.php
@@ -0,0 +1,40 @@
+<?php
+/*
+// Nome do host da base de dados
+define('HOSTNAME', 'localhost');
+
+// Banco de dados
+define('DB_TYPE', 'mysql');
+
+// Nome do DB
+define('DB_NAME', 'singep');
+
+// Usuário do DB - modifique para seu usuário do banco
+define('DB_USER', 'root');
+
+// Senha do DB - modifique para sua senha do banco
+define('DB_PASSWORD', '');
+
+// Charset da conexão PDO
+define('DB_CHARSET', 'utf8');
+/*
+//{"login":"true","email":"edson@uol.com.br","documento":"123456"}
+*/
+
+// Nome do host da base de dados
+define('HOSTNAME', 'localhost:3306');
+
+// Banco de dados
+define('DB_TYPE', 'mysql');
+
+// Nome do DB
+define('DB_NAME', 'u930453410_api');
+
+// Usuário do DB - modifique para seu usuário do banco
+define('DB_USER', 'root');
+
+// Senha do DB - modifique para sua senha do banco
+define('DB_PASSWORD', '');
+
+// Charset da conexão PDO
+define('DB_CHARSET', 'utf8');
